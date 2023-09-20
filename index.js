68cc3814b45f4ded48eadb1a9fb23c75ce592a1d
AlexDev2808 <colores.2808.8000@gmail.com>
2023-09-20 09:49:50 -0600

    Recibir parametros

--- index.js
+++ index.js
@@ -7,6 +7,43 @@ app.get("/", function (req, res) {
   res.send('Hola mi server en express');
 });
 
+app.get("/nueva-ruta", function (req, res) {
+  res.send('Hola, nueva ruta');
+});
+
+app.get("/products", function (req, res) {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000,
+    },
+    {
+      name: 'Producto 2',
+      price: 1400,
+    }
+  ])
+});
+
+app.get('/products/:id', function(req, res) {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Producto aleatorio',
+    price: 1000,
+  })
+});
+
+app.get('/categories/:categoryId/products/:productId', function(req, res) {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  })
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log("Escuchando en el port: " + port);
