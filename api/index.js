26cb9590fdcc4d1635ecc2a2b576093940dd8220
alexDev2808 <colores.2808.8000@gmail.com>
2023-10-06 12:19:46 -0600

    Haciendo cambios para subir proyecto a Vercel

--- api/index.js
+++ api/index.js
@@ -5,14 +5,14 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); //middleware
 
-const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+const whitelist = ['http://localhost:3000', 'https://myapp.com'];
 const options = {
   origin: ( origin, callback ) => {
-    if ( whitelist.includes( origin ) ) {
+    if ( whitelist.includes( origin ) || !origin ) {
       callback(null, true)
     } else {
       callback(new Error('No permitido'));
@@ -21,11 +21,11 @@ const options = {
 }
 app.use(cors(options));
 
-app.get("/", function (req, res) {
+app.get("/api", function (req, res) {
   res.send('Hola mi server en express');
 });
 
-app.get("/nueva-ruta", function (req, res) {
+app.get("/api/nueva-ruta", function (req, res) {
   res.send('Hola, nueva ruta');
 });
 
